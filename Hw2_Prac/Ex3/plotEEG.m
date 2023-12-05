5a1205f0caf7a1bee1b1aec207a70e726df34a89
Amirreza Hatamipour <arhp2000@gmail.com>
2023-12-05 11:21:07 +0330

    Add files via upload

---
+++ Hw2_Prac/Ex3/plotEEG.m
@@ -0,0 +1,10 @@
+function  plotEEG(X,text)
+%X  % 32-channel data
+load('Electrodes') ;
+% Plot Data
+% Use function disp_eeg(X,offset,feq,ElecName)
+offset = max(abs(X(:))) ;
+feq = 250 ;
+ElecName = Electrodes.labels ;
+disp_eeg(X,offset,feq,ElecName,text);
+end
